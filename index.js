f9607e3c4406d9f1a51419bc90a5ca7d3309ca04
github username <github email address>
2020-03-26 10:15:44 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+fetch('http://localhost:3000/api/v1/users', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  },
+  body: JSON.stringify({
+    user: {
+      username: "sylviawoods",
+      password: "whatscooking",
+      bio: "Sylvia Woods was an American restaurateur who founded the sould food restaurant Sylvia's in Harlem on Lenox Avenue, New York City in 1962. She published two cookbooks and was an important figure in the community.",
+      avatar: "https://upload.wikimedia.org/wikipedia/commons/4/49/Syvia_of_Sylvia%27s_reaturant_N.Y.C_%28cropped%29.jpg"
+    }
+  })
+})
+  .then(r => r.json())
+  .then(console.log)
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,5 +1,5 @@
 15.times do
   User.create(
-    username: Faker::Friends.character, password: 'hi', avatar: Faker::Fillmurray.image, bio: Faker::Friends.quote
+    username: Faker::Name.unique.name, password: 'hi', avatar: Faker::Fillmurray.image, bio: Faker::ChuckNorris.fact
   )
 end
